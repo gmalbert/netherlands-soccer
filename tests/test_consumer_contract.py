7079cb39852fab0bcf5508d266d40ab74c75deed
gmalbert <gmalbert+github@gmail.com>
2026-09-09 23:43:37 -0400

    test: synchronize shared core contract pin

--- tests/test_consumer_contract.py
+++ tests/test_consumer_contract.py
@@ -6,7 +6,7 @@ from scripts.precompute_predictions import _weather_enabled
 
 
 ROOT = Path(__file__).resolve().parents[1]
-CORE_REF = "4b0a7ed36324c52a6e9d968764d82c336ec301df"
+CORE_REF = "eb7750c5fd033213051511723e35ec253c3f1ca8"
 CORE_VERSION = "1.4.0"
 
 
